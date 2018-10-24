1c3940578d450b9890aafeb90171d9334afd4c9c
hemajaikumar <hema.jaikumar2004@gmail.com>
2018-10-24 12:27:36 +0530

    created a class to show a message

---
+++ src/SampleGitOne.java
@@ -0,0 +1,21 @@
+
+public class SampleGitOne {
+
+	/**
+	 * @param args
+	 */
+	public void displayMessage(){
+		System.out.println("Welcome to the GIT Code Management tool");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		SampleGitOne sample = new SampleGitOne();
+		sample.displayMessage();
+		
+		
+	}
+
+}
